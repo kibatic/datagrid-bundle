eb4d8b19e43652247582b8ed9834b769930cf901
JCrombez <jeremy.crombez@kibatic.com>
2022-08-04 16:03:19 +0200

    add simple array column type template

--- src/Grid/Template.php
+++ src/Grid/Template.php
@@ -7,5 +7,6 @@ class Template
     const TEXT = 'text.html.twig';
     const DATETIME = 'datetime.html.twig';
     const BOOLEAN = 'boolean.html.twig';
+    const ARRAY = 'array.html.twig';
     const ACTIONS = 'actions.html.twig';
 }
