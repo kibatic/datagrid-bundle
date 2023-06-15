05d6644572d1d94b0cfdb6fc84913ebc2399aba3
Jérémy CROMBEZ <777666+jcrombez@users.noreply.github.com>
2023-06-15 15:13:24 +0200

    Update KibaticDatagridExtension.php

--- src/DependencyInjection/KibaticDatagridExtension.php
+++ src/DependencyInjection/KibaticDatagridExtension.php
@@ -10,7 +10,7 @@ use Symfony\Component\DependencyInjection\Reference;
 
 class KibaticDatagridExtension extends Extension
 {
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         (new YamlFileLoader(
             $container,
