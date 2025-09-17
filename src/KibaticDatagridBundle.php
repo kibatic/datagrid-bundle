4a5f159be7db9c6184dcce1dc96d1c1a6443cbf2
jcrombez <jeremy.crombez@kibatic.com>
2025-09-17 14:03:58 +0200

    load bundle extension

--- src/KibaticDatagridBundle.php
+++ src/KibaticDatagridBundle.php
@@ -2,8 +2,14 @@
 
 namespace Kibatic\DatagridBundle;
 
+use Kibatic\DatagridBundle\DependencyInjection\KibaticDatagridExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class KibaticDatagridBundle extends Bundle
 {
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        return new KibaticDatagridExtension();
+    }
 }
