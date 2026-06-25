3230da65b0c5451f2a0164b92f3255f0dd738d0a
Renaud <renaud.bredy@kibatic.com>
2026-06-25 11:36:06 +0200

    Filtres datagrid : groupes de champs et filtrage temps réel
    - Groupes de filtres (filterLayout) rendus côte à côte (.datagrid-filter-group)
    - Filtrage temps réel via relatedTurboFrames (form-turbo-frame-updater),
      sans bouton submit quand actif
    - Classe kibatic-datagrid propagée au composant de filtres
    - Retire les constantes de thème BOOTSTRAP4 et BOOTSTRAP4_SONATA

--- src/Grid/Theme.php
+++ src/Grid/Theme.php
@@ -6,6 +6,4 @@ class Theme
 {
     const KIBATIC = '@KibaticDatagrid/theme/kibatic';
     const BOOTSTRAP5 = '@KibaticDatagrid/theme/bootstrap5';
-    const BOOTSTRAP4 = '@KibaticDatagrid/theme/bootstrap4';
-    const BOOTSTRAP4_SONATA = '@KibaticDatagrid/theme/bootstrap4-sonata';
 }
